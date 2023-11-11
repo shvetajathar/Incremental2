91030594027a7ff0891885046ba4546c20a676a4
Iamneo <techadmin@iamneo.ai>
2023-11-11 05:06:14 +0000

    Backup Commit

--- dotnetapp/Program.cs
+++ dotnetapp/Program.cs
@@ -11,6 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+var connectionString=builder.Configuration.GetConnectionString("mycon");
+builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(connectionString));
 
 // Add your DbContext and Identity services
 // ...
